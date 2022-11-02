f5632428cd8eed4447acaddbf765d155830334ed
M Zubair <md.zubair143598@gmail.com>
2022-11-02 18:17:10 +0500

    init

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Components/Elements/Hero'
+import Faqs from '../Components/Utils/Faqs'
+
+const Home = () => {
+  return (
+    <>
+    <Hero/>
+    <Faqs/>
+    </>
+  )
+}
+
+export default Home
